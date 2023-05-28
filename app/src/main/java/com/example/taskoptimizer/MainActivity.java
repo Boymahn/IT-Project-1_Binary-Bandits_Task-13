a9d945be5ebee4289a404b4f39d46c48d4792dea
Mason Evans-Billington <z5317443@ad.unsw.edu.au>
2023-05-28 21:58:40 +1000

    connecting to database not working yet

--- app/src/main/java/com/example/taskoptimizer/MainActivity.java
+++ app/src/main/java/com/example/taskoptimizer/MainActivity.java
@@ -3,106 +3,122 @@ package com.example.taskoptimizer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.button.MaterialButton;
-
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView username;
-    private TextView password;
-    private MaterialButton loginbtn;
-    private MaterialButton signupbtn;
-    private DatabaseConnection databaseConnection;
+    private EditText username, password;
+    private Button loginButton, signupButton;
+    private static final String DB_HOSTNAME = "taskoptimizer.mysql.database.azure.com";
+    private static final String DB_USERNAME = "myadminuser";
+    private static final String DB_PASSWORD = "BinaryBandits69";
+    private static final String DB_NAME = "taskoptimizer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        databaseConnection = new DatabaseConnection();
-        databaseConnection.connectToDatabase(); // Connect to the database
-
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
-        loginbtn = findViewById(R.id.loginbtn);
-        signupbtn = findViewById(R.id.signupbtn);
+        loginButton = findViewById(R.id.loginbtn);
+        signupButton = findViewById(R.id.signupbtn);
 
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    login(username.getText().toString(), password.getText().toString());
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                String name = username.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+
+                if (!name.isEmpty() && !pass.isEmpty()) {
+                    login(name, pass);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        signupbtn.setOnClickListener(new View.OnClickListener() {
+        signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSignUpPage();
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
             }
         });
     }
 
-    public void login(String name, String pass) throws SQLException {
-        Connection connection = databaseConnection.getConnection();
+    private void login(String u, String p) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
 
-        if (connection == null) {
-            // Handle connection error
-            Toast.makeText(MainActivity.this, "Failed to connect to the database", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        // Create a PreparedStatement object with parameter placeholders
-        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-
-        // Set the parameter values
-        preparedStatement.setString(1, name);
-        preparedStatement.setString(2, pass);
-
-        // Execute the query
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-        // Check if a matching user and password combination exists
-        if (resultSet.next()) {
-            // Correct credentials
-            Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-            // Open another activity after successful login
-            openDashboardPage();
-        } else {
-            // Incorrect credentials
-            Toast.makeText(MainActivity.this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
-        }
-
-        // Close the result set, statement, and connection
         try {
-            resultSet.close();
-            preparedStatement.close();
-            connection.close();
-        } catch (SQLException e) {
+            // Load the MySQL Connector/J driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Create the database connection URL
+            String url="jdbc:mysql://taskoptimizer.mysql.database.azure.com:3306/taskoptimizer?useSSL=true";
+
+            // Create the database connection
+            connection = DriverManager.getConnection(url, DB_USERNAME, DB_PASSWORD);
+            Toast.makeText(MainActivity.this, "here3", Toast.LENGTH_SHORT).show();
+
+            // does not reach here
+            if (connection == null) {
+                // Handle connection error
+                Toast.makeText(MainActivity.this, "Failed to connect to the database", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+            }
+
+            // Create the SQL query
+            String query = "SELECT * FROM users WHERE username = '" + u + "' AND password = '" + p + "'";
+
+            // Create the statement
+            statement = connection.createStatement();
+
+            // Execute the query
+            resultSet = statement.executeQuery(query);
+
+            // Check if the result set is not empty
+            if (resultSet.next()) {
+                Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                // Redirect to the next activity
+                Intent intent = new Intent(MainActivity.this, NavBarControl.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(MainActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+            }
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+        } finally {
+            // Close the result set, statement, and connection
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-    }
-
-    private void openDashboardPage() {
-        Intent intent = new Intent(MainActivity.this, NavBarControl.class);
-        startActivity(intent);
-    }
-
-    private void openSignUpPage() {
-        Intent intent = new Intent(MainActivity.this, SignUp.class);
-        startActivity(intent);
     }
 }
