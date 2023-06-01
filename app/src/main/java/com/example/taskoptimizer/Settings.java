6966493a8eef6252302e002d21058fe1b18ed82f
Mason Evans-Billington <z5317443@ad.unsw.edu.au>
2023-06-01 16:06:47 +1000

    themes

--- app/src/main/java/com/example/taskoptimizer/Settings.java
+++ app/src/main/java/com/example/taskoptimizer/Settings.java
@@ -3,19 +3,41 @@ package com.example.taskoptimizer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import com.google.android.material.button.MaterialButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Settings extends AppCompatActivity {
 
     private MaterialButton logoutbtn;
+    private Switch themeSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Apply the default theme
+        setTheme(R.style.LightTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         logoutbtn = findViewById(R.id.logoutbtn);
+        themeSwitch = findViewById(R.id.themeSwitch);
+
+        themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Apply the selected theme based on the switch state
+                if (isChecked) {
+                    setTheme(R.style.DarkTheme);
+                } else {
+                    setTheme(R.style.LightTheme);
+                }
+
+                // Recreate the activity to reflect the theme change
+                recreate();
+            }
+        });
 
         logoutbtn.setOnClickListener(new View.OnClickListener() {
             @Override
