aa654d59020c98409ade5c84ef7f48b9bc9adf90
OliverPunt <114469222+OliverPunt@users.noreply.github.com>
2023-06-06 13:38:30 +1000

    Focus timer to block notifications - Allow access to settings

--- app/src/main/java/com/example/taskoptimizer/FocusTimer.java
+++ app/src/main/java/com/example/taskoptimizer/FocusTimer.java
@@ -71,7 +71,7 @@ public class FocusTimer {
     }
 } */
 
-package com.example.taskoptimizer;
+/* package com.example.taskoptimizer;
 
 import android.app.NotificationManager;
 import android.content.ComponentName;
@@ -150,6 +150,103 @@ public class FocusTimer {
             }
         }
     }
+    public void disableDoNotDisturb() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (isDoNotDisturbEnabled()) {
+                NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                if (notificationManager != null) {
+                    notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
+                }
+            }
+        }
+    }
+
+    public boolean isDoNotDisturbEnabled() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (notificationManager != null) {
+                return notificationManager.isNotificationPolicyAccessGranted() &&
+                        notificationManager.getCurrentInterruptionFilter() != NotificationManager.INTERRUPTION_FILTER_ALL;
+            }
+        }
+        return false;
+    }
+} */
+
+package com.example.taskoptimizer;
+
+import android.app.NotificationManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.provider.Settings;
+
+public class FocusTimer {
+    private Context context;
+    private CountDownTimer timer;
+    private long focusTimeInMillis;
+
+    public FocusTimer(Context context) {
+        this.context = context;
+    }
+
+    public boolean isNotificationPolicyAccessGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (notificationManager != null) {
+                return notificationManager.isNotificationPolicyAccessGranted();
+            }
+        }
+        return false;
+    }
+
+    public void requestNotificationPolicyAccess() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!isNotificationPolicyAccessGranted()) {
+                Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                context.startActivity(intent);
+            }
+        }
+    }
+
+    public void startFocusTimer(long focusTimeInMillis) {
+        this.focusTimeInMillis = focusTimeInMillis;
+        enableDoNotDisturb();
+        timer = new CountDownTimer(focusTimeInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Timer is ticking (1 second interval)
+                long minutesLeft = millisUntilFinished / (60 * 1000);
+                // Update UI or perform actions for each tick
+            }
+
+            @Override
+            public void onFinish() {
+                // Timer finished
+                disableDoNotDisturb();
+                // Perform actions after the focus time is over
+            }
+        }.start();
+    }
+
+    public void stopFocusTimer() {
+        if (timer != null) {
+            timer.cancel();
+            disableDoNotDisturb();
+        }
+    }
+
+    public void enableDoNotDisturb() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!isDoNotDisturbEnabled()) {
+                Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                context.startActivity(intent);
+            }
+        }
+    }
+
     public void disableDoNotDisturb() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (isDoNotDisturbEnabled()) {
